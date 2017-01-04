f7aaf4fb70673679581fca7b195048f57316f918
Suvankar Mazumder <smazumder@gmail.com>
2017-01-04 09:31:12 -0500

    Update loader.s

--- src/stage2/loader.s
+++ src/stage2/loader.s
@@ -17,11 +17,11 @@ mov esp, kernel_stack + KERNEL_STACK_SIZE  	; point esp to the start of the stac
 
 section .text:                  ; start of the code section
 align 4                         ; the code must be 4 bytes aligned
-	dd MAGIC_NUMBER             ; write the magic number to the machine code
+	dd MAGIC_NUMBER         ; write the magic number to the machine code
     	dd FLAGS
     	dd CHECKSUM                 
 
 loader:                         ; the loader symbol defined as the entry point to the linker script
-	mov eax, 0xCAFEBABE          ; place the special number in the eax register
+	mov eax, 0xCAFEBABE     ; place the special number in the eax register
 .loop:
-jmp .loop                    ; now loop forever
+jmp .loop                    	; now loop forever
